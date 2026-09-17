aaf710ab7c651bb9dcdbdce639bffa726ebf8343
Matt <matthewniellaquino@gmail.com>
2026-09-17 11:59:22 +0800

    Add files via upload

--- aquino_act2b/admin/students/index.php
+++ aquino_act2b/admin/students/index.php
@@ -1,3 +1,19 @@
+<?php
+
+session_start();
+include "../../config/database.php";
+
+// only admin users can access this page
+if(!isset($_SESSION["role"]) || $_SESSION["role"] != "admin"){
+    header("location: ../../index.php");
+    exit;
+}
+$sql = "SELECT * FROM users WHERE role ='student' ORDER BY id DESC";
+$result = mysqli_query($conn,$sql);
+
+?>
+
+
 <!doctype html>
 <html lang="en">
 
@@ -42,6 +58,9 @@
 
     <!-- Main Content -->
     <div class="container py-4">
+        <?php if(isset($_GET ["message"])){?>
+        <div class="alert alert-succes"><?php echo $_GET["message"];?></div>
+        <?php }?>
 
         <!-- Header Section -->
         <div class="d-flex justify-content-between mb-3">
@@ -49,14 +68,14 @@
             <div>
                 <h2>Student Accounts</h2>
 
-                <a href="dashboard.html">
+                <a href="../dashboard.php">
                     ← Dashboard
                 </a>
             </div>
 
             <a
                 class="btn btn-primary"
-                href="student_form.html"
+                href="create.php"
             >
                 + Add Student
             </a>
@@ -81,15 +100,16 @@
                     <tbody>
 
                         <!-- Student Record -->
+                         <?php while($row = mysqli_fetch_assoc($result)){?>
                         <tr>
-                            <td>2026-0001</td>
+                            <td><?php echo htmlspecialchars($row ["student_no"]);?></td>
 
                             <td>
-                                Juan Dela Cruz
+                                <?php echo htmlspecialchars($row ["full_name"]);?>
                             </td>
 
                             <td>
-                                juan
+                                <?php echo htmlspecialchars($row ["username"]);?>
                             </td>
 
                             <td>
@@ -114,6 +134,7 @@
                                 </button>
                             </td>
                         </tr>
+                        <?php }?>
 
                     </tbody>
 
